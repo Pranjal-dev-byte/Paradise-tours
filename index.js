8e6602520824ed852cfbf2fd97af3f18de7555e3
pranjal <pranjalmisra2410@gmail.com>
2020-07-14 02:47:48 +0530

    Added minor functionality change

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const labelName = document.querySelector('#name');
 const inputLabel = document.querySelector('#form__label--name');
 const labelName1 = document.querySelector('#email');
 const inputLabel1 = document.querySelector('#form__label--email');
-const popupBtn = document.querySelector('#btn-popup');
 const popupClass = document.querySelector('.popup');
 const closePopup = document.querySelector('#popup-close');
+const popupBtn2 = document.querySelectorAll('.btn-popup');
 
 labelName.addEventListener('keyup', (e) => {
 	if (labelName.value) {
-		console.log(labelName.value);
+		// console.log(labelName.value);
 		inputLabel.innerHTML = `<label for="name">Full name:</label>`;
 		inputLabel.classList.remove('u-hidden');
 	} else {
@@ -17,18 +17,19 @@ labelName.addEventListener('keyup', (e) => {
 });
 labelName1.addEventListener('keyup', (e) => {
 	if (labelName1.value) {
-		console.log(labelName1.value);
+		// console.log(labelName1.value);
 		inputLabel1.innerHTML = `<label for="email">Email address:</label>`;
 		inputLabel1.classList.remove('u-hidden');
 	} else {
 		inputLabel1.classList.add('u-hidden');
 	}
 });
-popupBtn.addEventListener('click', () => {
-	console.log('clicked');
-	popupClass.classList.remove('u-hidden');
-});
+for (let element of popupBtn2) {
+	element.addEventListener('click', () => {
+		popupClass.classList.remove('u-hidden');
+	});
+}
 closePopup.addEventListener('click', () => {
-	console.log('clicked');
+	// console.log('clicked');
 	popupClass.classList.add('u-hidden');
 });
