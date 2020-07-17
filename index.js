39b86c403467016f0f1eb1c8cef8068f89025469
pranjal <pranjalmisra2410@gmail.com>
2020-07-17 08:55:45 +0530

    Added animation for booking form

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-const labelName = document.querySelector('#name');
-const inputLabel = document.querySelector('#form__label--name');
-const labelName1 = document.querySelector('#email');
-const inputLabel1 = document.querySelector('#form__label--email');
+const labelName = document.querySelector('#label-name');
+const inputName = document.querySelector('#input-name');
+const labelEmail = document.querySelector('#label-email');
+const inputEmail = document.querySelector('#input-email');
 const popupClass = document.querySelector('.popup');
 const closePopup = document.querySelector('#popup-close');
 const popupBtn2 = document.querySelectorAll('.btn-popup');
 
-labelName.addEventListener('keyup', (e) => {
-	if (labelName.value) {
-		// console.log(labelName.value);
-		inputLabel.innerHTML = `<label for="name">Full name:</label>`;
-		inputLabel.classList.remove('u-hidden');
-	} else {
-		inputLabel.classList.add('u-hidden');
-	}
+inputName.addEventListener('focus', () => {
+	labelName.classList.add('form__label--animate');
 });
-labelName1.addEventListener('keyup', (e) => {
-	if (labelName1.value) {
-		// console.log(labelName1.value);
-		inputLabel1.innerHTML = `<label for="email">Email address:</label>`;
-		inputLabel1.classList.remove('u-hidden');
-	} else {
-		inputLabel1.classList.add('u-hidden');
-	}
+inputEmail.addEventListener('focus', () => {
+	labelEmail.classList.add('form__label--animate');
 });
 for (let element of popupBtn2) {
 	element.addEventListener('click', () => {
